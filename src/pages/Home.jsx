4293bbc739dfa2d832bd6c8ca80160468e1cf465
Anselme Dor <anselmedor@hotmail.be>
2023-03-07 18:31:16 +0100

    Reworking UX in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom";
 import { Parallax } from "react-scroll-parallax";
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(true);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setLoaded(false);
+      setLoaded(true);
     }, 600);
   }, []);
 
   return (
     <section
       id="#"
-      className="min-h-[80vh] md:min-h-screen max-w-2xl flex flex-col justify-center items-center md:items-start gap-8 md:gap-16 font-roboto overflow-hidden"
+      className="min-h-screen max-w-2xl flex flex-col justify-center items-center sm:items-start gap-16 font-roboto overflow-hidden"
     >
       <h1
         className={`text-secondary font-sourceCodePro text-2xl ml-2 transition duration-500 delay-150 ${
-          loaded ? "opacity-0 translate-y-4" : "translate-y-0"
+          loaded ? "translate-y-0" : "opacity-0 translate-y-4"
         }`}
       >
         Hi, I'm
       </h1>
       <div
-        className={`text-4xl md:text-8xl font-bold  transition duration-500 delay-300 font-roboto ${
-          loaded ? "opacity-0 translate-y-4" : "translate-y-0"
+        className={`text-4xl sm:text-8xl font-bold  transition duration-500 delay-300 font-roboto ${
+          loaded ? "translate-y-0" : "opacity-0 translate-y-4"
         }`}
       >
         <h2>ANSELME</h2>
@@ -35,8 +35,8 @@ export default function Home() {
       </div>
 
       <h3
-        className={`text-gray-400 text-center md:text-left md:text-xl transition duration-500 delay-[450ms] ${
-          loaded ? "opacity-0 translate-y-4" : "translate-y-0"
+        className={`text-gray-400 text-center sm:text-left sm:text-xl transition duration-500 delay-[450ms] ${
+          loaded ? "translate-y-0" : "opacity-0 translate-y-4"
         }`}
       >
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
@@ -45,21 +45,29 @@ export default function Home() {
         rerum. A eveniet totam, neque perspiciatis saepe enim, quasi porro,
         veritatis maxime tempora corporis.
       </h3>
-
       <div
-        className={` text-secondary transition duration-500  font-sourceCodePro delay-[600ms]  ${
-          loaded ? "opacity-0 translate-y-4 " : "translate-y-0"
+        className={`transition duration-500 delay-[600ms] ${
+          loaded ? "translate-y-0" : "opacity-0 translate-y-4 "
         }`}
       >
-        <Link
-          to={"/resume"}
-          className="px-6 md:px-12 py-4 hover:bg-secondary/10 transition duration-300 delay-[0] border border-secondary"
+        <div
+          className={` py-4 text-secondary border border-secondary  font-sourceCodePro  btn__home`}
+          id="btn"
         >
-          check out my resume
-        </Link>
+          <Link
+            to={"/resume"}
+            className="px-6 md:px-12 py-4 transition duration-500 hover:text-primary"
+          >
+            check out my resume
+          </Link>
+        </div>
       </div>
 
-      <div className={`hidden lg:block absolute max-w-sm right-0`}>
+      <div
+        className={`hidden lg:block absolute max-w-sm right-0 transition duration-150 ${
+          loaded ? "opacity-1" : "opacity-0"
+        }`}
+      >
         <Parallax speed={-30}>
           <Player autoplay loop src="/assets/LaptopLottie.json" />
         </Parallax>
