ff4ab713a9ea4dafe0de4eb4e54b1b118ad6afa5
Anselme Dor <anselmedor@hotmail.be>
2023-03-08 16:11:30 +0100

    Adding about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,83 @@
 import { Player } from "@lottiefiles/react-lottie-player";
-import React from "react";
+import React, { useEffect } from "react";
 import { Parallax } from "react-scroll-parallax";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function About() {
+  useEffect(() => {
+    AOS.init();
+  });
   return (
     <section
       className="min-h-screen flex flex-col justify-center items-center"
       id="about"
     >
-      <h4>About Me</h4>
+      <div className="grid grid-cols-1 sm:grid-cols-2 text-white text-left gap-16">
+        <div className="flex flex-col justify-center items-center gap-16 font-roboto uppercase text-4xl ">
+          <div data-aos="fade-down">
+            <h4 className="glitch">About Me</h4>
+            <h4 className="glitch">About Me</h4>
+            <h4 className="glitch">About Me</h4>
+          </div>
+          <div className="shine" data-aos="fade-down">
+            <figure className="rounded-full overflow-hidden w-1/2 mx-auto">
+              <img
+                src="/assets/Anselme.png"
+                alt=""
+                className="bg-white/75 rounded-full"
+              />
+            </figure>
+          </div>
+        </div>
+        <div className="font-sourceCodePro flex flex-col gap-4">
+          <p data-aos="fade-up">
+            My name is{" "}
+            <span className="font-bold text-secondary">Anselme Dor</span>, I'm a
+            junior full stack developer currently completing my training
+            @BeCode.
+          </p>
+          <p data-aos="fade-up">
+            My experience @BeCode led me to learn various aspects of{" "}
+            <span className="font-bold text-secondary">web development</span>{" "}
+            but also working in teams.
+          </p>
+          <p data-aos="fade-up">
+            I had the opportunity to do serveral{" "}
+            <a
+              href="#work"
+              className="hover-underline-animation font-bold text-secondary animate-pulse"
+            >
+              projects
+            </a>{" "}
+            (in groups / alone) and not only learn the theory.
+          </p>
+          <p data-aos="fade-up">
+            I am passionate about creating beautiful websites with intuitive and
+            easy to use <span className="font-bold text-secondary">UI</span>.
+          </p>
+          <p data-aos="fade-up">
+            I'm currently looking for an internship to complete my training
+            @BeCode. Interested in working with me ? You can always{" "}
+            <a
+              href="#contact"
+              className="hover-underline-animation font-bold text-secondary animate-pulse"
+            >
+              contact me
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+      <div className="hidden absolute lg:flex left-20">
+        <Parallax speed={-20}>
+          <img
+            src="/assets/RoundTextPurple.png"
+            alt="Round text"
+            className="w-[30%] animate-spinSlow"
+          />
+        </Parallax>
+      </div>
     </section>
   );
 }
