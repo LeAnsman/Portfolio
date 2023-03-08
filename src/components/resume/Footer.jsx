825f9f3fc7526a8535bdb3aff4e92aecc14b1392
Anselme Dor <anselmedor@hotmail.be>
2023-03-08 19:49:51 +0100

    Reworking resume

--- src/components/resume/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div>
-      <a href="https://www.mockofun.com">Design created with MockoFUN</a>
-    </div>
-  );
-}
