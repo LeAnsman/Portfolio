3acd5d27cc8506001e528a3e4090b872361fb736
Anselme Dor <anselmedor@hotmail.be>
2023-03-09 18:01:26 +0100

    Removing aos footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,12 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <footer
-      className="relative w-full bg-secondary/10 text-white py-4"
-      data-aos="fade-in"
-      data-aos-anchor-placement="center-bottom"
-    >
+    <footer className="relative w-full bg-secondary/10 text-white py-4">
       <div className="flex justify-between mx-10">
         <p className="text-center text-sm">© 2023 Anselme Dor.</p>
-        <SocialLinks />
+        <div className="flex gap-8">
+          <SocialLinks size={24} />
+        </div>
       </div>
     </footer>
   );
