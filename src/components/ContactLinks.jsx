740a61d61b25affb7c8366a02ae244f40961dc1a
Anselme Dor <anselmedor@hotmail.be>
2023-03-09 18:01:51 +0100

    adding contact links

---
+++ src/components/ContactLinks.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SocialLinks from "./SocialLinks";
+
+export default function ContactLinks() {
+  return (
+    <div data-aos="fade-left">
+      <p className="font-sourceCodePro text-xl text-white text-center">
+        you can also <span className="font-bold text-secondary">contact</span>{" "}
+        me on my <span className="font-bold text-secondary">socials</span>
+      </p>
+      <div className="flex justify-center gap-20 sm:gap-40 py-8 text-white">
+        <SocialLinks size={75} />
+      </div>
+    </div>
+  );
+}
