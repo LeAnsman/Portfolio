d19b2950bf1991c5238f0b6d5dc05397c43dec4f
Anselme Dor <anselmedor@hotmail.be>
2023-03-10 10:33:24 +0100

    Add tags to card and bg img + animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 
-export default function Card({ title, link, firstTag, secondTag }) {
+export default function Card({
+  title,
+  link,
+  firstTag,
+  secondTag,
+  thirdTag,
+  fourthTag,
+  backgroundImg,
+}) {
   return (
     <a
       href={link}
       target="_blank"
-      className="relative text-white  font-sourceCodePro"
+      className="relative text-white font-sourceCodePro bg-cover bg-center group transition duration-300 lg:hover:translate-y-16"
+      style={{
+        backgroundImage: `url(${backgroundImg})`,
+      }}
     >
-      <p className="absolute left-5 top-2 text-primary font-medium bg-secondary px-4 py-2 rounded-lg">
+      <p className="absolute left-5 top-2 font-medium bg-secondary/50 px-4 py-2 rounded-lg text-sm transition duration-500 lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:-translate-y-16">
         {firstTag}
       </p>
-
-      <p className="absolute right-5 top-2 text-primary font-medium bg-secondary px-4 py-2 rounded-lg">
+      <p className="absolute right-5 top-2 font-medium bg-secondary/50 px-4 py-2 rounded-lg text-sm transition duration-500 lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:-translate-y-16">
         {secondTag}
       </p>
-      <div className="relative h-72 xl:h-80 w-56 bg-primary border border-secondary transition duration-300 cursor-pointer flex justify-center items-center hover:translate-y-16">
+      <p className="absolute left-5 bottom-2 font-medium bg-secondary/50 px-4 py-2 rounded-lg text-sm transition duration-300  lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:z-10">
+        {thirdTag}
+      </p>
+      <p className="absolute right-5 bottom-2 font-medium bg-secondary/50 px-4 py-2 rounded-lg text-sm transition duration-300  lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:z-10">
+        {fourthTag}
+      </p>
+      <div className="lg:relative h-72 lg:h-80 w-72 lg:w-56 bg-primary/50  border border-secondary cursor-pointer flex justify-center items-center">
         <p className="text-center">{title}</p>
       </div>
     </a>
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -12,30 +12,43 @@ export default function Work() {
         <h4 className="glitch">MY WORK</h4>
         <h4 className="glitch">MY WORK</h4>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mt-4">
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-4 mb-28 sm:mb-0"
+        data-aos="fade-right"
+      >
         <Card
           title={"school helper"}
           link={"https://school-helper-app-tsdr7.ondigitalocean.app/"}
+          backgroundImg={"assets/images/SchoolHelper.png"}
           firstTag={"react"}
-          secondTag={"front"}
+          secondTag={"ui/ux"}
+          thirdTag={"laravel"}
+          fourthTag={"front"}
         />
         <Card
           title={"r3f landing page"}
           link={"/"}
-          firstTag={"r3f"}
-          secondTag={"front"}
+          firstTag={"react"}
+          secondTag={"r3f"}
+          thirdTag={"3d"}
+          fourthTag={"front"}
         />
         <Card
           title={"aircnc"}
           link={"https://airc-n-c.vercel.app/"}
-          firstTag={"js"}
-          secondTag={"full"}
+          firstTag={"react"}
+          secondTag={"nodejs"}
+          thirdTag={"learning"}
+          fourthTag={"full"}
         />
         <Card
           title={"stockz"}
           link={"https://stockz.vercel.app/"}
-          firstTag={"js"}
-          secondTag={"full"}
+          backgroundImg={"assets/images/StockZ.png"}
+          firstTag={"react"}
+          secondTag={"redux"}
+          thirdTag={"learning"}
+          fourthTag={"full"}
         />
       </div>
     </section>
