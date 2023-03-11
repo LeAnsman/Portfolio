c0a95f371a82fd019ea0fa968e5d0897612d743f
Anselme Dor <anselmedor@hotmail.be>
2023-03-11 15:23:13 +0100

    UX update

--- src/components/CanvasContainer.jsx
+++ src/components/CanvasContainer.jsx
@@ -5,7 +5,7 @@ import { OrbitControls } from "@react-three/drei";
 
 export default function CanvasContainer() {
   return (
-    <Canvas className="sm:!w-1/2 !h-[80vh] pt-20">
+    <Canvas className="sm:!w-1/2 !h-[80vh] pt-20 cursor-grab active:cursor-grabbing">
       <Scene />
       <OrbitControls enableZoom={false} />
     </Canvas>
